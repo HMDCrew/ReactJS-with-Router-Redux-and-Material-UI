0029965edb7854d9039d27d5409b11f3bd252e2f
HMDCrew <andrei523070@gmail.com>
2021-09-28 12:26:27 +0200

    Initial commit

---
+++ src/components/pages/VerifyEmail.js
@@ -0,0 +1,23 @@
+/*import React,{useState} from 'react';
+import { Button, TextField, Card } from "@material-ui/core";
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {useStyles} from '../../styles/styles';
+import {useDispatch, useSelector} from 'react-redux';
+import {LoginAction} from '../../redux/actions/AuthActions';
+import {useHistory,Link} from 'react-router-dom'; 
+
+function VerifyEmail() {
+
+    const classes = useStyles();
+
+    return (
+        <div>
+            <div className={classes.centerItem}>
+                <Card>
+                    <h2><b>Welcome to VerifyEmail</b></h2>
+                </Card>
+            </div>
+        </div>
+    )
+}
+export default VerifyEmail;*/
